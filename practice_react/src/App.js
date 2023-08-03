0cd9ab24d99ddf95fe136b9c928bb3cf5bca9f9b
x-xiho <x_xiho3o@naver.com>
2023-08-03 19:31:46 +0900

    embeding ID URL Token
    아이디랑 URL 토큰 추가해서 임베딩 성공

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
+import React from 'react';
 
 
   function App() {
@@ -12,10 +13,10 @@ import { models } from 'powerbi-client';
           <PowerBIEmbed
             embedConfig={{
               type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-              id: undefined,
-              embedUrl: undefined,
-              accessToken: undefined,
-              tokenType: models.TokenType.Embed, // Use models.TokenType.Aad for SaaS embed
+              id: "d0a102a6-6d81-4844-ac91-79f494b9cb7e",
+              embedUrl: "https://app.powerbi.com/reportEmbed?reportId=d0a102a6-6d81-4844-ac91-79f494b9cb7e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUVBU1QtQVNJQS1BLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19",
+              accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZmQwOWIyYmMtOTIyMC00YzZiLTgzNzItMjIwYjdiZDUxODE5LyIsImlhdCI6MTY5MTA1NjM5MSwibmJmIjoxNjkxMDU2MzkxLCJleHAiOjE2OTEwNjEwNTYsImFjY3QiOjAsImFjciI6IjEiLCJhZ2VHcm91cCI6IjQiLCJhaW8iOiJBVFFBeS84VUFBQUEvTkVpcTJsUW5pUzIrOXU1L3VkNk04UEtuRDNzTVR1bjlPTDA4RGxkSksvcmhvVFB0ZVNnUkswK3BXNFNEcEVqIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImZhbWlseV9uYW1lIjoiS2ltIiwiZ2l2ZW5fbmFtZSI6IkppaG8iLCJpcGFkZHIiOiIyMjEuMTU0LjI0LjgxIiwibmFtZSI6IktpbUppaG8iLCJvaWQiOiIxYzc0NjdhNC0xYjMyLTRiMjUtYTZiMS0wNjUxNTg4ZTUwM2EiLCJwdWlkIjoiMTAwMzIwMDA0OTAzNkM2RiIsInJoIjoiMC5BWEVBdkxJSl9TQ1NhMHlEY2lJTGU5VVlHUWtBQUFBQUFBQUF3QUFBQUFBQUFBQnhBTVUuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoicFZ3N2VHcW1hb2NUcVl5X0ZRdGFhZUlnb3dDOHNZSlUyb0RQdDIwVXVWayIsInRpZCI6ImZkMDliMmJjLTkyMjAtNGM2Yi04MzcyLTIyMGI3YmQ1MTgxOSIsInVuaXF1ZV9uYW1lIjoia29uZzg4QGR1a3N1bmcuYWMua3IiLCJ1cG4iOiJrb25nODhAZHVrc3VuZy5hYy5rciIsInV0aSI6IlFJTkJ6Y3gwOEVpZEY1eGYyVDJQQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfcGwiOiJrby1LUiJ9.YcGywcwG38x9hL5k0gvRdYHjjdMNAJTlyPf1qm_RpsOX1jYBZ6i-xO_qSkMiPnLlF6flV7NY7lTk75dHlm64nlOtrWlw5lKHlq19RsjuAAhsc2bVRF696eB4Ji5Yk7RBtQ3FUH80IjpFAT71mpZdATMDS5zg8t58z1TaMUPoCWREz-LwkcWRUh7XFUs49ohuURezcJgLYuvHxMuJr9pH2upHmtarcu9i5AP2eCSebMhSoEzEkX59eMvL5ENmajvnFL4p0GsVeZ7DnYvN78bsbhYvE5PT4tDl4NRlnTx-Hyo6cBQBIPziAIaGpZyzSDKl7wnMKaw8qr34EeaOKn2hEg",
+              tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
               settings: {
                 panes: {
                   filters: {
@@ -23,6 +24,7 @@ import { models } from 'powerbi-client';
                     visible: true
                   }
                 },
+                background: models.BackgroundType.Transparent,
                 
               }
             }}
